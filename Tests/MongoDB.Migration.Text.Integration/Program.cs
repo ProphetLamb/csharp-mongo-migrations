57a8f7c5afe1ef4b26f96798849c9ff8335aded3
ProphetLamb <prophet.lamb@gmail.com>
2023-12-21 13:01:56 +0100

    Add test service

--- Tests/MongoDB.Migration.Text.Integration/Program.cs
+++ Tests/MongoDB.Migration.Text.Integration/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MongoDB.Migration;
+using MongoDB.Migration.Core;
+using MongoDB.Migration.Text.Integration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .Configure<DatabaseSettings>(o => { })
+    .AddMigrations()
+    .AddHostedService<TestService>();
+
+var app = builder.Build();
+app.Run();
+
+namespace MongoDB.Migration.Text.Integration
+{
+    public sealed class DatabaseSettings : IMongoMigratable
+    {
+        public const string Alias = "TestDatabase";
+
+        public string DatabaseName { get; init; } = $"TestDatabase{Environment.CurrentManagedThreadId}";
+
+        public MongoMigrableDefinition GetMigratableDefinition()
+        {
+            return new()
+            {
+                ConnectionString = "mongodb://localhost:27017",
+                Database = new(Alias, DatabaseName),
+                MirgrationStateCollectionName = "MIGRATION_COLLECTION"
+            };
+        }
+    }
+
+    public sealed class TestService(IMongoMigrationCompletion completion) : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                CancellationTokenSource cts = new(TimeSpan.FromSeconds(10));
+                var result = await completion.WaitAsync(DatabaseSettings.Alias, cts.Token).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                Environment.Exit(1);
+            }
+            Environment.Exit(0);
+        }
+    }
+}
